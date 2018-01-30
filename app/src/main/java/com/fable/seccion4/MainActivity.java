f4f8a913dc28f50bda607ffab0613744daf7f625
fabian.leguizamo <fabian.leguizamo@eytdesarrollos.net>
2018-01-29 21:25:47 -0500

    Añadir Funcionalidad a Elementos UI Parte 2

--- app/src/main/java/com/fable/seccion4/MainActivity.java
+++ app/src/main/java/com/fable/seccion4/MainActivity.java
@@ -3,18 +3,26 @@ package com.fable.seccion4;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void Llamada(View v) {
-        int a = 4;
-        Toast.makeText(this, "Botón Presionado " + a, Toast.LENGTH_LONG).show();
+        btn = (Button)findViewById(R.id.BotonPrincipal);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = 4;
+                Toast.makeText(MainActivity.this, "Botón Presionado " + a, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
